2a398696e5d14d09204aa9de41746eb46eb66208
hzhuo8880 <hao.grrr@gmail.com>
2026-04-13 21:23:02 +0900

    remove old tests, data samples, and unused files

--- src/test-setup.ts
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom/vitest";
-
-// CodeMirror uses ResizeObserver internally; jsdom doesn't provide it
-class MockResizeObserver {
-  observe() {}
-  unobserve() {}
-  disconnect() {}
-}
-global.ResizeObserver = MockResizeObserver;
